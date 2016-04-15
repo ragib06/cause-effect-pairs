1c5280673fcb971e51c4fe0a64e0cec8ed909b4e
Ragib Ahsan <ragib.ahsan@gmail.com>
2016-04-15 13:23:04 -0500

    added command line arguments

--- train.py
+++ train.py
@@ -9,6 +9,7 @@ from sklearn.pipeline import Pipeline
 from sklearn.svm import LinearSVC
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.cross_validation import KFold
+import sys, getopt
 
 def feature_extractor():
     features = [
@@ -19,8 +20,8 @@ def feature_extractor():
                 ('B: Normalized Entropy', 'B', f.SimpleTransform(transformer=f.normalized_entropy)),
                 ('Pearson R', ['A','B'], f.MultiColumnTransform(f.correlation)),
                 ('Pearson R Magnitude', ['A','B'], f.MultiColumnTransform(f.correlation_magnitude)),
-                ('Entropy Difference', ['A','B'], f.MultiColumnTransform(f.entropy_difference)),
-                ('Mutual Information', ['A', 'B'], f.MultiColumnTransform(transformer=f.normalized_mutual_information)),
+                ('Entropy Difference', ['A','B'], f.MultiColumnTransform(f.entropy_difference))#,
+                # ('Mutual Information', ['A', 'B'], f.MultiColumnTransform(transformer=f.normalized_mutual_information)),
                 ]
     combined = f.FeatureMapper(features)
     return combined
@@ -29,8 +30,8 @@ def get_pipeline():
     features = feature_extractor()
     steps = [("extract_features", features),
              # ("classify", MultinomialNB())]
-             ("classify", LinearSVC(loss='hinge'))]
-             # ("classify", RandomForestRegressor(n_estimators=50, n_jobs=3, min_samples_split=10, random_state=1))]
+             # ("classify", LinearSVC(loss='hinge'))]
+             ("classify", RandomForestRegressor(n_estimators=50, n_jobs=3, min_samples_split=10, random_state=1))]
     return Pipeline(steps)
 
 
@@ -67,22 +68,50 @@ def crossvalidate(data, num_fold):
 
 
 def main():
+
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "n:c:h")
+    except getopt.GetoptError as err:
+        print str(err)
+        sys.exit(2)
+
+    numRows = None
+    cv = False
+    nfold = 10
+
+    clamp = lambda n, minn, maxn: max(min(maxn, n), minn)
+    for o, a in opts:
+        if o == "-n":
+            numRows = clamp(int(a), 1, 4050)
+        elif o == "-c":
+            cv = True
+            nfold = int(a)
+        elif o == "-h":
+            print "python train.py -n [number of rows]"
+            print "python train.py -c [number of folds]"
+        else:
+            print "try help: python train.py -h"
+            pass
+
+
     print("Reading in the training data")
-    train = data_io.read_train_pairs()
-    target = data_io.read_train_target()
+    train = data_io.read_train_pairs(numRows)
+    target = data_io.read_train_target(numRows)
 
-    data = {}
-    data['train'] = train
-    data['target'] = target
+    if cv:
+        data = {}
+        data['train'] = train
+        data['target'] = target
 
-    crossvalidate(data, 10)
+        print "Initiating " + str(nfold) + " fold cross validation ..."
+        crossvalidate(data, nfold)
+    else:
+        print("Extracting features and training model")
+        classifier = get_pipeline()
+        classifier.fit(train, target.Target)
 
-    # print("Extracting features and training model")
-    # classifier = get_pipeline()
-    # classifier.fit(train, target.Target)
-    #
-    # print("Saving the classifier")
-    # data_io.save_model(classifier)
+        print("Saving the classifier")
+        data_io.save_model(classifier)
     
 if __name__=="__main__":
     main()
